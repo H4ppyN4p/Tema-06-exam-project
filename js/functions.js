e69a7c71e9605e389b76afbd9c5b010e645754fe
H4ppyN4p <tobi5703@stud.kea.dk>
2025-06-11 02:36:29 +0200

    Finished menu, finished phone and tablet look for index.html, added functions for the menu

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,14 @@
 const openMenuBtn = document.querySelector("#navfolded")
 const closeMenuBtn = document.querySelector("#navopened")
+const themeBtn = document.querySelector("#temaer")
+
 
 
 openMenuBtn.addEventListener("click", openDialog)
 closeMenuBtn.addEventListener("click", closeDialog)
+themeBtn.addEventListener("click", toggleThemes)
+
+var isThemesToggled = false
 
 const menuDialog = document.querySelector("#menu")
 
@@ -13,4 +18,24 @@ function openDialog() {
 
 function closeDialog() {
     menuDialog.close()
+}
+
+function toggleThemes() {
+    console.log ("toggling themes")
+    if (isThemesToggled) {
+        isThemesToggled = false
+        document.getElementById("nav-arrow-head").style.transform = "rotate(0deg)"
+        document.getElementById("nav-about-me").style.top = "0"
+        document.getElementById("temaer-nav").style.display = "none"
+
+
+    } else {
+        isThemesToggled = true
+        document.getElementById("nav-arrow-head").style.transform = "rotate(270deg)"
+        document.getElementById("nav-about-me").style.top = "120px"
+        document.getElementById("temaer-nav").style.display = "block"
+
+
+
+    }
 }
